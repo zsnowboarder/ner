41092663125f638c5862ae003b8532bb923e3fda
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-12-12 23:51:19 -0800

    Update ner.py

--- ner.py
+++ ner.py
@@ -26,6 +26,8 @@ nlp.add_pipe("find_dates", before="ner")
 # In[56]:
 
 
+st.title("NER Radact")
+st.write("This is a proof of concept of using Name Entity Recognition (NER) an approach leveraging Natural Language Processing extract key information for processing. There are many use cases. Redacting is only one example.")
 text = """On December 10, 2024 at 1200h, Victim John was walking on the street in 300 block Main Street. Suspect Tom assaulted John. Witness Mary (1997/01/01) called police. PC 9204 Dave Thompson arrived and arrested suspect. Police found a gun and a knife after searching."""
 
 text = st.text_area(label="Enter your synopsis", value=text)
@@ -45,12 +47,9 @@ def replace_entities(doc):
             prev_ent = ent.label_ 
     return new_text
 
-# Replace entities in the text
-replaced_text = replace_entities(doc)
-#print(replaced_text)
-st.write("New text:\n" + replaced_text)
-
-
+if st.button(label="Redact"):
+    replaced_text = replace_entities(doc)
+    st.write("Redacted text:\n" + replaced_text)
 # for ent in doc.ents:
 #     print(ent.text, ent.label_)
 
